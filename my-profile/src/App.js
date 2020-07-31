fb06e3508fbb51a2d081f28aa08406156ecfb5e8
biraj.borah <biraj.borah@FireEye.com>
2020-07-31 20:40:02 +0530

    adding the initial project

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
+import React ,{Fragment,useState,useEffect}from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [pfImage, setpfImage] = useState("");
+  const [pfName, setpfName] = useState("");
+  const [pfNickName, setpfNickName] = useState("");
+  const [pfDesignation, setpfDesignation] = useState("");
+
+  const [repoContent, setrepoContent] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.github.com/users/supreetsingh247',
+      );
+      console.log(result.data);
+      setpfImage(result.data.avatar_url);
+      setpfName(result.data.name);
+      setpfNickName(result.data.login);
+      setpfDesignation(result.data.bio);
+    };
+ 
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    const fetchData1 = async () => {
+      const result1 = await axios(
+        'https://api.github.com/users/supreetsingh247/repos',
+      );
+      setrepoContent(result1.data);
+     
+    };
+ 
+    fetchData1();
+  }, []);
+
+  const items = []
+  if (repoContent.length > 0){
+    for (const [index, value] of repoContent.entries()) {
+      items.push(<h1 key={index}>{value.name}</h1>)
+    }
+  }
+  
+  return(
+      
+      <Fragment>
+        
+        <div>
+          <a>
+            <img src={pfImage} style={{width: "200px"}}></img>
+          </a>
+          <div>
+           <h1>{pfName}</h1>
+           <span>{pfNickName}</span>
+           <p>{pfNickName}</p>
+           <h3>{pfDesignation}</h3>
+          </div>
+
+        </div>
+         <div>
+         {items}
+        
+
+         </div>
+       
+      </Fragment>
+      
+  )
 }
 
 export default App;
